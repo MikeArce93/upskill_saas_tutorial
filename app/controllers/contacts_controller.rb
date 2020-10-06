43aa885206b7bb0fbd93de0ed235cdc84cdefe59
Mike Arce <michael.arce93@gmail.com>
2020-10-06 03:58:24 +0000

    Fixed problems with contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,12 +9,12 @@ class ContactsController < ApplicationController
             name = params[:contact][:name]
             email = params[:contact][:email]
             body = params[:contact][:comments]
-            ContactMailers.contact_email(name, email, body).deliver
+            ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = 'Message sent.'
-         redirect_to new_contact_path
+         redirect_to new_contact_path_url
     else 
             flash[:danger] = @contact.errors.full_messages.join(", ")
-         redirect_to new_contact_path 
+         redirect_to new_contact_path_url
         end
     end
     
